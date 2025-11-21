37efa2beeb4d425aaebf7fb00c9568e672de0416
MEHDI MANSOURI <pioneering.meti@gmail.com>
2025-11-21 14:48:19 +0100

    Initial commit: Spring Boot skeleton

---
+++ src/test/java/com/klipsan/mhdmansouri/backend/KlipsanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klipsan.mhdmansouri.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlipsanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
